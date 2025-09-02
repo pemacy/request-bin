c78756b3f6d154d6614ac93792e4c707c273692e
Preston Macy <preston.macy@gmail.com>
2025-09-02 16:52:08 -0400

    initialized form component, added uuid npm package

---
+++ client/src/components/Form.tsx
@@ -0,0 +1,10 @@
+import * as webhookApi from '../services/webhookApi'
+import { v4 as uuidv4 } from 'uuid'
+
+const Form = () => {
+  return (
+    <form action="" onSubmit={handleOnSubmit}>
+      <input type='text' value=/>
+    </form>
+  )
+}
