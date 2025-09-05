0acb0fd03ecd62c354b77c96648f5e9f0d66773a
Preston Macy <preston.macy@gmail.com>
2025-09-05 09:04:23 -0400

    hover and popup effect in bin header

--- client/src/components/BinPage/BinPageHeader.tsx
+++ client/src/components/BinPage/BinPageHeader.tsx
@@ -41,23 +41,36 @@ const removeBin = async (
 }
 
 
-const BinPageHeader = ({ setBins, selectedBin, setView, setSelectedBin, setRecords } : BinPageHeaderProps) => {
+const BinPageHeader = ({ setBins, selectedBin, setView, setSelectedBin, setRecords }: BinPageHeaderProps) => {
   return (
-       <div className="grid grid-cols-10 rounded-xl bg-white p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <span className="col-start-1 col-end 4">
-          <a onClick={() => setView('home')}>Home</a>
-        </span>
-        <span className="col-start-9 col-end-9">
-          <button onClick={(e) => removeRecords(e, selectedBin, setView, setSelectedBin, setRecords)}>
-            <RiFileShredLine style={{ color: 'grey', fontSize: '24px' }} />
-          </button>
-        </span>
-        <span className="col-start-10">
-          <button onClick={(e) => removeBin(e, setBins, selectedBin, setView, setSelectedBin, setRecords)}>
-            <FcReuse style={{ color: 'yellow', fontSize: '24px' }} />
-          </button>
-        </span>
-      </div>
+    <div className="grid grid-cols-10 rounded-xl bg-white p-4 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <span className="col-start-1 col-end 4">
+        <a
+          className="text-xl text-gray-300 transition-all duration-200 ease-in-out hover:text-blue-500 hover:scale-110 hover:cursor-pointer"
+          onClick={() => setView('home')}>Home</a>
+      </span>
+      <span className="col-start-9 col-end-9">
+        <button onClick={(e) => removeRecords(e, selectedBin, setView, setSelectedBin, setRecords)}>
+          <div className="relative group inline-block">
+            <RiFileShredLine className="text-2xl text-gray-500 transition-all duration-200 ease-in-out hover:text-blue-500 hover:scale-110 cursor-pointer" />
+            <div className="absolute left-1/2 -translate-x-1/2 mt-2 px-2 py-1 text-xs text-white bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              Delete Records
+            </div>
+          </div>
+        </button>
+      </span>
+      <span className="col-start-10">
+        <button onClick={(e) => removeBin(e, setBins, selectedBin, setView, setSelectedBin, setRecords)}>
+
+          <div className="relative group inline-block">
+            <FcReuse className="text-2xl text-yellow-400 transition-all duration-200 ease-in-out hover:text-blue-500 hover:scale-110 hover:cursor-pointer" />
+            <div className="absolute left-1/2 -translate-x-1/2 mt-2 px-2 py-1 text-xs text-white bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              Delete Bin
+            </div>
+          </div>
+        </button>
+      </span>
+    </div>
   )
 }
 
